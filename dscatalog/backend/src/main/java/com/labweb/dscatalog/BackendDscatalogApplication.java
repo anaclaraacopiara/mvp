025caad04d1f4c2f4492c54538a38c9c5d817137
anaclaraacopiara <anaclaraacopiara@gmail.com>
2022-05-25 09:54:40 -0300

    first commit

---
+++ dscatalog/backend/src/main/java/com/labweb/dscatalog/BackendDscatalogApplication.java
@@ -0,0 +1,13 @@
+package com.labweb.dscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDscatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDscatalogApplication.class, args);
+	}
+
+}
